acc80bb596271db627e165a03fe6b2d350eb8dec
Vumus <109515943+Vumus@users.noreply.github.com>
2022-07-19 16:32:49 +0700

    Delete src/src directory

--- src/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import home from './home';
-
-test('renders learn react link', () => {
-  render(<home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
